7e8b9068d44d0284ccfb30c2fc304630820fad84
stewie <stewie@syneforge.com>
2021-02-04 01:13:14 +0200

    test

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.get('/lol', (req, res) => {
+  res.status(418)
+  res.set({ 'X-her': 'her' })
+  res.send()
+})
+
 app.get('/', (req, res) => {
   res.send('ALO EBAT  !')
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
